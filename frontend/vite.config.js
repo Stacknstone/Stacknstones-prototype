fecaa04e4f21ec2095f5c59c7257f59bc0852796
ProficientPug <rohith200345@gmail.com>
2025-11-11 09:19:46 +0530

    adding sitemap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import sitemap from 'vite-plugin-sitemap';
 // https://vite.dev/config/
 export default defineConfig({
-  // This is the line you need to change:
-  base: '/', 
-  plugins: [react()],
-})
+  // This is the line you need to change:
+ base: '/', 
+ plugins: [react(), sitemap({hostname: 'https://www.stacknstones.in/'}),],
+});
